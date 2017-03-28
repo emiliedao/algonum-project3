b17c64056a136b5be8d9a9a64ec1ec8ff1073023
Emilie <emilie.dao@enseirb-matmeca.fr>
2017-03-28 12:14:06 +0200

    continued bidiagonal, separated tests, added svd

---
+++ bidiagonal_tests.py
@@ -0,0 +1,8 @@
+from bidiagonal import *
+
+# Tests
+
+A = np.matrix( [[1, 5, 3, 2],
+                [3, 6, 4, 1],
+                [2, 1, 0, 7]] )
+bidiagonal(A)
---
+++ svd_tests.py
@@ -0,0 +1,3 @@
+from svd import *
+
+# SVD tests
