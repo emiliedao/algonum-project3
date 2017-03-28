413fe6662a50eed4415a90f1cc7c78d51c2d2f68
JUVEN Alexis <ajuven@caillasse.pedago.ipb.fr>
2017-03-28 12:25:31 +0200

    product AxH

--- householder.py
+++ householder.py
@@ -26,7 +26,7 @@ def optimized_vector_product(U,X):
     return X - 2*U*np.transpose(U)*X
 
 
-def optmized_matrix_product(U,A):
+def optimized_matrix_product_HxA(U,A):
     assert(len(U) == len(A))
     assert((np.linalg.norm(U) - 1.0)<eps)
 
@@ -34,3 +34,44 @@ def optmized_matrix_product(U,A):
     for i in range(A.shape[1]):
             product_result[:, i] = optimized_vector_product(U,A[:, i])
     return product_result
+
+
+def optimized_matrix_product_AxH(A,U):
+    n = len(A)
+    m = len(U)
+    print n
+    print m
+    print A.shape[1]
+    assert(m == A.shape[1])
+    assert((np.linalg.norm(U) - 1.0)<eps)
+
+    product_result = np.matrix(np.zeros([n,m]))
+    for i in range(m):
+        v = optimized_vector_product(U,np.reshape(A[i, :],[n,1]))
+        product_result[i, : ] = np.reshape(v,[1,n])
+    return product_result
+
+
+
+
+
+
+X = np.array([[1.0],[2.0],[0.0]])
+Y = np.array([[0.0],[math.sqrt(5)],[0.]])
+
+
+A = np.matrix( [[1, 5, 3],
+                [3, 6, 4],
+                [2, 1, 0]] )
+
+
+print A
+U = calculate_householder_vector(X,Y)
+print U
+H = householder_vector_to_matrix(U)
+print H
+
+print "n"
+print optimized_matrix_product_AxH(A,U)
+print "\n"
+print A*H
