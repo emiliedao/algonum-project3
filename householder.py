3f14b413ec13c25492758efb800b7580751fa56c
ilyas el guato <elguato.ilyas@gmail.com>
2017-03-28 11:02:05 +0200

    Add files via upload

--- householder.py
+++ householder.py
@@ -1,49 +1,54 @@
 import numpy as np;
-
-def calculate_U(X, Y, eps=1e-10):
-    n = X.size
+import random;
+import math;
+eps=1e-10
+def calculate_householder_vector(X, Y):
+    assert(len(X) == len(Y))
+    n = len(X)
     U = np.matrix(np.zeros([n,1]))
-
     norm = np.linalg.norm(X - Y)
-
     # X != Y
     if norm > eps:
         U = np.dot(1/norm, X - Y)
-
+    assert((np.linalg.norm(U) - 1.0)<eps)
     return U
-
-
-def calculate_householder(U):
-    n = U.size
+def householder_vector_to_matrix(U):
     return np.matrix(np.eye(n,n)) - 2*U*np.transpose(U)
 
+def optimized_vector_product(U,X):
+    assert(len(U) == len(X))
+    assert((np.linalg.norm(U) - 1.0)<eps)
 
-def calculate_optimized_householder(U, X):
-    lbda = np.transpose(X)*U
-    return X - 2*lbda[0,0]*U
-
-
-def householder(X, Y):
-    return calculate_householder(calculate_U(X, Y))
+    return X - 2*U*np.transpose(U)*X
 
 
-# Builds the matrix [ [Id, 0], [0, H] ] of size n
-#def reshape_householder(H, n):
-#    A = np.eye(H.size/2,
+def optmized_matrix_product(U,A):
+    assert(len(U) == len(A))
+    assert((np.linalg.norm(U) - 1.0)<eps)
+
+    product_result = np.matrix(np.zeros([len(U), A.shape[1]]))
+    for i in range(A.shape[1]):
+            product_result[:, i] = optimized_vector_product(U,A[:, i])
+    return product_result
+#tests
+X = np.matrix([[3.0], [4.0], [0.0]])
+Y = np.matrix([[0.0], [0.0], [5.0]])
+n = len(X)
+m = random.randint(1,10)
+U = calculate_householder_vector(X,Y)
+H = householder_vector_to_matrix(U)
+optimized_HX = optimized_vector_product(U,X)
+print "\nHouseholder vector U:\n", U
+print "\nHouseholder Matrix H:\n", H
+print "\nOptimized product of H, the Householder matrix of U, and X:\n", optimized_HX
+A = np.matrix(np.zeros([n, m]))
+for i in range(n+1):
+    A[:,i] = X
+print "\nOptimized product of H, the Householder matrix of U, and A a matrix whose ", A.shape[1] ," columns are all equal to X\n", optmized_matrix_product(U, A)
+assert(np.linalg.norm(H*X - Y) < eps)
+assert(np.linalg.norm(optimized_vector_product(U,X) - H*X) < eps)
 
 
-##################################
-# Tests
 
-X = np.matrix([3.0, 4, 0]) .T
-Y = np.matrix([0, 0, 5]) .T
-
-U = np.matrix(calculate_U(X, Y))
-print "Matrix U :\n", U
-
-H = calculate_householder(U)
-print "\nHouseholder :\n", H
-
-V = np.matrix([1,0,0]) . T
-S = calculate_optimized_householder(U, V)
-print "\nOptimized Householder :\n", S
+print np.linalg.norm(U)
+print U
