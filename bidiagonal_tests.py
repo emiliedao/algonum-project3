81748bbcc83d0fbb0b082cc4d74493b05d5f81ec
Emilie Dao <emilie@MacBook-Pro-de-Emilie.local>
2017-04-01 00:48:45 +0200

    tested bidiagonal with random matrix

--- bidiagonal_tests.py
+++ bidiagonal_tests.py
@@ -1,15 +1,10 @@
 from bidiagonal import *
-from print_matrix import *
+from matrix import *
 
-# Tests
-
-# to do : generate random matrix
-A = np.matrix( [[1, 5],
-                [3, 6],
-                [2, 1]] )
+A = generate_matrix()
 
 (L,BD,R) = bidiagonal(A)
+
 print_m(BD)
-print "\n\n\n"
-print BD
-print L*BD*R
+assert(is_bidiagonal(BD))
+assert(equal_m(L*BD*R, A))
