d2f8d948ad6ea585840aaab847ca59272530e68f
Ilyas EL GUATO <elguato.ilyas@gmail.com>
2017-04-01 20:43:50 +0200

    finished householder tests

--- householder_tests.py
+++ householder_tests.py
@@ -1,5 +1,5 @@
 from householder import *
-
+from scipy.linalg import qr
 # Householder tests
 
 X = np.matrix([[3.0], [4.0], [0.0]])
@@ -48,3 +48,39 @@ print "n"
 print optimized_matrix_product_AxH(A,U)
 print "\n"
 print A*H
+
+
+print " begin tests\n"
+n = random.randint(3,9)
+def get_random_ortho_vector(n):
+    H = np.random.randn(n, n)
+    Q, R = qr(H)
+    return Q[:,0].reshape(n,1)
+def get_test_vector(n):
+    return np.matrix(np.random.randint(4, size=n)).reshape(n,1)
+def get_test_matrix(n):
+    for i in range(n):
+        A[:,i] = get_test_vector(n)
+    return A
+
+A = np.matrix(np.zeros([n,n]))
+nb_tests = 668
+for i in range(nb_tests):
+    A = get_test_matrix(n)
+    U = get_random_ortho_vector(n)
+    H = householder_vector_to_matrix(U)
+    print "\t\tRandom Householder Matrix H:\n",H
+    optimized_HA = optimized_matrix_product_HxA(U,A)
+    optimized_AH = optimized_matrix_product_AxH(A,U)
+    AH = A*H
+    HA = H*A
+    assert(np.linalg.norm(optimized_AH - AH) < eps)
+    assert(np.linalg.norm(optimized_HA - HA) < eps)
+    print "\n\n\t\tOptimized product A*H\n",optimized_AH
+    print "\n\n\t\tRegular product A*H\n", AH
+    print "\n\n\t\tOptimized product H*A\n",optimized_HA
+    print "\n\n\t\tRegular product H*A\n", HA
+    print " \n\n\n\t\t\tTest number:",nb_tests," Passed\n\n"
+
+
+print "\n\n\n\t\t\t\tEnd of tests\n"
